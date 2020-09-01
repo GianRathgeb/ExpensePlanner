04c6a291f039995722d0f36a1309d5f02336f46a
gianrathgeb <gian.rathgeb@bbz-sh.ch>
2020-09-01 15:08:21 +0200

    Fixed total sum of day. small typo in if clause

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -16,7 +16,7 @@ class Chart extends StatelessWidget {
       for (var i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekDay.day &&
             recentTransactions[i].date.month == weekDay.month &&
-            recentTransactions[i].date.month == weekDay.year) {
+            recentTransactions[i].date.year == weekDay.year) {
           totalSum += recentTransactions[i].amount;
         }
       }
